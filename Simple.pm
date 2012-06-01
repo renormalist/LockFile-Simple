dad0eaaa7211c1a94040ef6d41d0ad45e6ac3708
Steffen Schwigon <ss5@renormalist.net>
2012-06-01 22:17:00 +0200

    fix defined(@array) deprecation with Perl 5.16

--- Simple.pm
+++ Simple.pm
@@ -132,8 +132,8 @@ sub make {
 	$self->{'lock_by_file'} = {};
 
 	# The logxxx routines are autoloaded, so need to check for @EXPORT
-	$self->{'wfunc'} = defined(@Log::Agent::EXPORT) ? \&logwarn : \&core_warn;
-	$self->{'efunc'} = defined(@Log::Agent::EXPORT) ?  \&logerr  : \&core_warn;
+	$self->{'wfunc'} = @Log::Agent::EXPORT ? \&logwarn : \&core_warn;
+	$self->{'efunc'} = @Log::Agent::EXPORT ?  \&logerr  : \&core_warn;
 
 	$self->configure(@hlist);		# Will init "manager" if necessary
 	return $self;
